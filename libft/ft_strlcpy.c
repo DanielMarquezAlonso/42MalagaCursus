1655ed23ef165fca974359c019de975a8e876430
Daniel Marquez Alonso <danmarqu@c3r9s2.42malaga.com>
2023-09-14 19:56:16 +0200

    mas

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,13 +6,21 @@
 /*   By: danmarqu <danmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 19:28:42 by danmarqu          #+#    #+#             */
-/*   Updated: 2023/09/14 19:33:16 by danmarqu         ###   ########.fr       */
+/*   Updated: 2023/09/14 19:55:05 by danmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 // #include <stdio.h>
-// #include <strings.h>
+// #include <string.h>
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{}
+// size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+// {}
+
+int main()
+{
+	char des[20] = "hola";
+	char src[20] = "casa";
+
+	printf(strlcpy(des, src));
+}
